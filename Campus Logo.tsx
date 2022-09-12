a67d649d76999a98cdc439037f0a3ac8c097992a
CampusHandwerkSWH <112859321+CampusHandwerkSWH@users.noreply.github.com>
2022-09-12 15:05:50 +0200

    Delete Campus Logo.tsx

--- Campus Logo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="Campus Logo" tilewidth="32" tileheight="32" tilecount="6" columns="6">
- <image source="C:/Users/Linda Wessendorf/Downloads/Campus Logo.png" width="192" height="32"/>
-</tileset>
