db70f2f053c1630e9351277b9a223dc9cbecc51e
CampusHandwerkSWH <112859321+CampusHandwerkSWH@users.noreply.github.com>
2022-09-14 15:06:49 +0200

    Erneut Logo eingefügt

---
+++ Campus_Logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Campus_Logo" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="tilesets/Campus_Logo.png" width="192" height="32"/>
+</tileset>
