34756e9b344574216a556becbb3572682743e045
CampusHandwerkSWH <112859321+CampusHandwerkSWH@users.noreply.github.com>
2022-09-14 15:40:22 +0200

    Wieder rückgängig

--- WA_campus_Logo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="WA_campus_Logo" tilewidth="32" tileheight="32" tilecount="6" columns="6">
- <image source="tilesets/WA_campus_Logo.png" width="192" height="32"/>
-</tileset>
